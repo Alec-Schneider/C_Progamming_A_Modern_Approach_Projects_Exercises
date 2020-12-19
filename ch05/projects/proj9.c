3093a3bb4a22681a6af9efc83ec258deb82b8765
Alec-Schneider <afs95@comcast.net>
2020-12-19 15:24:29 -0500

    Adding ch05

--- ch05/projects/proj9.c
+++ ch05/projects/proj9.c
@@ -6,14 +6,30 @@ int main()
     int m1, d1, y1,
         m2, d2, y2;
 
-    printf("Enter the first date (mm/dd/yy: ");
+    printf("Enter the first date (mm/dd/yy): ");
     scanf("%d/%d/%d", &m1, &d1, &y1);
-    printf("Enter the second date (mm/dd/yy: ");
+    printf("Enter the second date (mm/dd/yy): ");
     scanf("%d/%d/%d", &m2, &d2, &y2);
-    
-    if (y1 == y2){
-        if (y)
-    } else if ()
+
+    if (y1 < y2) {
+        printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m1, d1, y1, m2, d2, y2);
+    } else if (y1 > y2) {
+        printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m2, d2, y2, m1, d1, y1);
+    } else if (y1 == y2) {
+        if (m1 > m2){
+            printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m2, d2, y2, m1, d1, y1);
+        } else if (m1 < m2) {
+            printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m1, d1, y1, m2, d2, y2);
+        } else if (m1 == m2) {
+            if (d1 > d2) {
+                printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m2, d2, y2, m1, d1, y1);
+            } else if (d1 < d2) {
+                printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m1, d1, y1, m2, d2, y2);
+            } else {
+                printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m1, d1, y1, m2, d2, y2);
+            }
+        }
+    }
 
     return 0;
 }
